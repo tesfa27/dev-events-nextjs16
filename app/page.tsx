da7a2448b7e2ffec7d388df56fd20b28c92fe26a
tesfa <tesfamarkos27@gmail.com>
2025-11-12 16:40:53 +0300

    comply with v16 gods

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import ExploreBtn from './components/ExploreBtn'
 import EventCard from './components/EventCard'
 import { IEvent } from '@/database';
 import { cacheLife } from 'next/cache';
+import events from '@/lib/constants';
 
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
@@ -11,8 +12,8 @@ const Page = async () => {
     'use cache';
     cacheLife('hours')
 
-    const response = await fetch(`${BASE_URL}/api/events`);
-    const { events } = await response.json();
+    // const response = await fetch(`${BASE_URL}/api/events`);
+    // const { events } = await response.json();
     return (
         <section>
             <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
@@ -21,7 +22,7 @@ const Page = async () => {
             <div className='mt-20 space-y-7'>
                 <h3>Featured Events</h3>
                 <ul className='events'>
-                    {events && events.length > 0 && events.map((event: IEvent) => (
+                    {events && events.length > 0 && events.map((event) => (
                         <li key={event.title} className='list-none'>
                             <EventCard {...event} />
                         </li>
